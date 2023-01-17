ed8dcc23ef37a262b7bc02940f7cc2e6fecb84f8
Tonya-Ostapiuk <tonya.shevchik@gmail.com>
2023-01-17 21:40:18 +0300

    task 08-01 homepage/build

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const listGallery = document.querySelector('.gallery');
+const imageMarkup = createGallery (galleryItems);
+
+listGallery.insertAdjacentHTML('beforeend', imageMarkup);
+// listGallery.addEventListener('click', onGalleryContainerClick);
+
+function createGallery (galleryItems){
+  return galleryItems.map(({preview, original, description}) => {
+    return `
+    <div class="gallery__item">
+    <a class="gallery__item" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+        title="${description}"
+      />
+    </a>
+  </div>`;
+  }).join('');
+};
+
+const gallerySlide = new SimpleLightbox('.gallery a', {
+	captionDelay: 250
+});
+
 
 console.log(galleryItems);
